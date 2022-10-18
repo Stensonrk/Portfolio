eec875dd30a5db5ce2f030e01e3ce957f034d58f
Stensonrk <70451444+Stensonrk@users.noreply.github.com>
2022-10-18 10:08:15 -0500

    Delete Java Shapes Program directory

--- Java Shapes Program/TwoDimensionalShape.java
+++
@@ -1,9 +0,0 @@
-/*Robert Stenson
- * June 28, 2020
- * TwoDimensionalShape.java
- * Creates interface for 2D shapes, subclass of Shape */
-package Project1;
-
-public interface TwoDimensionalShape extends Shape {
-	public double getArea();
-}
